e1578f81fe8d54ddff6d58a28c474b1afd24343d
Meg Galvez <meg@oakmaine.com>
2019-06-24 16:35:08 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -6,5 +6,5 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUE
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("electronics", "business", 250000, 5-24-19, 6-01-19);
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("car", "business", 4000000, 5-15-19, 8-12-19);
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("model_airplanes", "entertainment", 8000, 5-15-19, 6-14-19);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("floors", "remodel", 7000, 6_-1-19, 6-10-19);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("floors", "remodel", 7000, 6-1-19, 6-10-19);
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("puppies", "entertainment", 100, 6-04-19, 6-10-19);
