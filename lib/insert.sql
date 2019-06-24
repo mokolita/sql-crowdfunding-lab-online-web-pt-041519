545365c8f910aa5309dda57e413bb09f234937f3
Meg Galvez <meg@oakmaine.com>
2019-06-24 16:54:33 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,11 +2,11 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUE
 ("bathroom", "remodel", 10000, 6-24-19, 7-28-19);
 ("dance_studio", "remodel", 100000, 9-19-19, 9-30-19);
 ("kitchen", "remodel", 30000, 6-24-19, 6-30-19);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("new_album", "entertainment", 2500, 5-24-19, 6-14-19);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("books", "entertainment", 100, 6-01-19, 6-10-19);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("electronics", "business", 250000, 5-24-19, 6-01-19);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("car", "business", 4000000, 5-15-19, 8-12-19);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("model_airplanes", "entertainment", 8000, 5-15-19, 6-14-19);
+("new_album", "entertainment", 2500, 5-24-19, 6-14-19);
+("books", "entertainment", 100, 6-01-19, 6-10-19);
+("electronics", "business", 250000, 5-24-19, 6-01-19);
+("car", "business", 4000000, 5-15-19, 8-12-19);
+("model_airplanes", "entertainment", 8000, 5-15-19, 6-14-19);
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("floors", "remodel", 7000, 6-1-19, 6-10-19);
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("puppies", "entertainment", 100, 6-04-19, 6-10-19);
 
