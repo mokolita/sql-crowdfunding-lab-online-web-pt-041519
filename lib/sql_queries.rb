9c5251a18567d71ca7d53c9602c729a1b52576ae
Meg Galvez <meg@oakmaine.com>
2019-06-24 17:39:19 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -40,7 +40,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 "SELECT projects.category, SUM(pledges.amount) AS total_pledge
 FROM projects
 JOIN pledges ON pledges.project.id = projects.id
-WHERE projects.category = "
+WHERE projects.category = 'music'; "
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
