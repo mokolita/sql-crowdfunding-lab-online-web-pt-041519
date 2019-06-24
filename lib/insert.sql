88632fe0b655490ef78bda26dbd7c97a71a09b82
Meg Galvez <meg@oakmaine.com>
2019-06-24 16:50:54 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,6 +8,7 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUE
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("model_airplanes", "entertainment", 8000, 5-15-19, 6-14-19);
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("floors", "remodel", 7000, 6-1-19, 6-10-19);
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("puppies", "entertainment", 100, 6-04-19, 6-10-19);
+
 INSERT INTO users (name, age) VALUES ("Bob", 68);
 INSERT INTO users (name, age) VALUES ("Donna", 67);
 INSERT INTO users (name, age) VALUES ("Meg", 29);
