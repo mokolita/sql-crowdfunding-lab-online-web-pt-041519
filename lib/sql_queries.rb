4c86fda45b66776f427c568f509403dbd2ea8df5
Meg Galvez <meg@oakmaine.com>
2019-06-24 17:37:25 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,11 +29,11 @@ HAVING met_goal >= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, SUM(pledges.amount)
+"SELECT users.name, SUM(pledges.amount) AS total_pledge
 FROM users
 JOIN pledges ON pledges.user_id = users.id
 GROUP BY users.name
-ORDER BY pledges.amount;"
+ORDER BY total_pledge;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
