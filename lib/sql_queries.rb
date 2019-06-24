b07425104527f2479a33ec88bf9ee1ccb1853c40
Meg Galvez <meg@oakmaine.com>
2019-06-24 17:39:05 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -37,7 +37,10 @@ ORDER BY total_pledge;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT projects.category, SUM(pledges.amount) AS total_pledge
+FROM projects
+JOIN pledges ON pledges.project.id = projects.id
+WHERE projects.category = "
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
