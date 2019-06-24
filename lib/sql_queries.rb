e35d32b3cf39ea4a606c6867fe72d83694b852f3
Meg Galvez <meg@oakmaine.com>
2019-06-24 17:33:23 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,7 +24,8 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 "SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) AS met_goal 
 FROM projects
 JOIN pledges ON pledges.project_id = projects.id
-WHERE SUM(pledges.amount) >= projects.funding_goal"
+GROUP BY projects.title
+HAVINGSUM(pledges.amount) >= projects.funding_goal"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
